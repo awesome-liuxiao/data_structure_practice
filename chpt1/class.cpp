35cd617a0858aee4c9e50c11321f50fec211f49f
Liu, Xiao <lio4072@hotmail.com>
2020-09-21 16:14:11 +0900

    a little class

---
+++ chpt1/class.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+class Passenger {
+    public:
+        Passenger();
+        bool isFrequentFlyer() const;
+        void makeFrequentFlyer(const string& newFreqFlyerNo);
+
+    priate:
+        string      name;
+        MealType    mealPref;
+        bool        isFreqFlyer;
+        string      freqFylerNo;
+};
+
